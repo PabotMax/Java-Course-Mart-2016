d4c76e46428399fa9d0c9886c1690b503927bc94
PabotMax <pabotmax@gmail.com>
2016-05-30 20:45:30 +0300

    Вызов методов
    Создание таблиц

--- src/app/App.java
+++ src/app/App.java
@@ -3,14 +3,35 @@ package app;
 import app.database.connection.DBUtils;
 
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
- * Created by mda on 5/13/16.
+ * Created by Family on 13.05.2016.
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Start");
-        Connection dbConnection = DBUtils.getDBConnection();
-        DBUtils.createAddressTable(dbConnection);
+    public static void main(String[] args) throws SQLException {
+        System.out.println ("Start");
+        /*Connection Connection = DBUtils.getDBConnection ();
+        DBUtils.createAddressTable (Connection);*/
+
+        /*Connection MySqlConnection = MySqlDB.getConnection ();
+        MySqlDB.createAddressTable (MySqlConnection);*/
+
+        Connection MySqlConnection = DBUtils.getConnection ();
+        Statement statement = MySqlConnection.createStatement ();
+
+        DBUtils.createMySqlContactTable (MySqlConnection);
+
+        DBUtils.createMySqlAddressTable (MySqlConnection);
+
+        DBUtils.createMySqlTelephoneNumbersTable (MySqlConnection);
+
+        DBUtils.createMySqlEmailsTable (MySqlConnection);
+
+
+
+
+
     }
 }
