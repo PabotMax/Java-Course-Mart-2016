7fefa80bffeda03fa8317431635b51277331d0d1
Pabot Max <pabotmax@gmail.com>
2016-06-06 15:34:11 +0300

    Create tables: contact, address, telephone_numbers, emails

--- src/app/database/connection/DBUtils.java
+++ src/app/database/connection/DBUtils.java
@@ -82,7 +82,7 @@ public class DBUtils {
         String login = "mysql";
         String password = "mysql_password";
 
-        return getConnection ("com.mysql.jdbc.Driver", url, login, password);
+        return getConnection("com.mysql.jdbc.Driver", url, login, password);
     }
 
     private static Connection getConnection(String dbDriver,
@@ -91,64 +91,46 @@ public class DBUtils {
                                             String dbPassword) {
         Connection connection = null;
         try {
-            Class.forName (dbDriver);
+            Class.forName(dbDriver);
         } catch (ClassNotFoundException e) {
-            System.out.println (e.getMessage ());
+            System.out.println(e.getMessage());
         }
         try {
-            connection = DriverManager.getConnection (dbConnection, dbUser, dbPassword);
+            connection = DriverManager.getConnection(dbConnection, dbUser, dbPassword);
             return connection;
         } catch (SQLException e) {
-            System.out.println (e.getMessage ());
+            System.out.println(e.getMessage());
         }
         return connection;
     }
-
-    static void update() {
-        String updateTableSQL = "UPDATE DBUSER SET USERNAME = 'mkyong_new' WHERE USER_ID = 1";
-
-        try {
-            Connection dbConnection = getConnection ();
-            Statement statement = dbConnection.createStatement ();
-
-            // выполняем запрос update SQL
-            statement.execute (updateTableSQL);
-
-            System.out.println ("Record is updated to DBUSER table!");
-        } catch (SQLException e) {
-            System.out.println (e.getMessage ());
-        }
-    }
-
 
     public static void createMySqlContactTable(Connection dbConnection) {
         Statement statement = null;
 
         String createTableSQL = "CREATE TABLE contact("
-                + "user_id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT, "
+                + "user_id INTEGER UNSIGNED NOT NULL, "
                 + "user_firstName VARCHAR(30) NOT NULL, "
                 + "user_lastName VARCHAR(20) NOT NULL, "
                 + "address_id INTEGER NOT NULL, "
-                + "telephone_numbers_id INTEGER NOT NULL, "
-                /*+ "emails VARCHAR(100) NOT NULL, "*/
+                + "telephone_number_id INTEGER NOT NULL, "
+                + "email_id VARCHAR(100) NOT NULL, "
                 + "birthday DATE NOT NULL, "
-                /*+ "photoPath VARCHAR(20) NOT NULL, "*/
                 + "PRIMARY KEY (user_id) "
                 + ")";
         try {
-            statement = dbConnection.createStatement ();
+            statement = dbConnection.createStatement();
 
             // выполнить SQL запрос
-            statement.execute (createTableSQL);
-            System.out.println ("Table \"dbuser\" is created!");
+            statement.execute(createTableSQL);
+            System.out.println("Table \"contact\" is created!");
         } catch (SQLException e) {
-            System.out.println (e.getMessage ());
+            System.out.println(e.getMessage());
         } finally {
             if (statement != null) {
                 try {
-                    statement.close ();
+                    statement.close();
                 } catch (SQLException e) {
-                    e.printStackTrace ();
+                    e.printStackTrace();
                 }
             }
         }
@@ -158,31 +140,36 @@ public class DBUtils {
     public static void createMySqlAddressTable(Connection dbConnection) {
         Statement statement = null;
 
+
         String createTableSQL = "CREATE TABLE address("
-                + "address_id INTEGER NOT NULL AUTO_INCREMENT, "
+                + "address_id INTEGER NOT NULL, "
                 + "country VARCHAR(30) NOT NULL, "
                 + "city VARCHAR(20) NOT NULL, "
                 + "street VARCHAR(250) NOT NULL, "
                 + "house_number INTEGER NOT NULL, "
                 + "house_suffix VARCHAR(20) NOT NULL, "
-                + "poste_code INTEGER NOT NULL, "
+                + "appartment INTEGER NOT NULL, "
+                + "post_code INTEGER NOT NULL, "
                 + "PRIMARY KEY (address_id) "
+                + "FOREIGN KEY (user_id) REFERENCES contact(user_id)  " +
+                "ON DELETE CASCADE " +
+                "ON UPDATE CASCADE "
                 + ")";
 
         try {
-            statement = dbConnection.createStatement ();
+            statement = dbConnection.createStatement();
 
             // выполнить SQL запрос
-            statement.execute (createTableSQL);
-            System.out.println ("Table \"dbuser\" is created!");
+            statement.execute(createTableSQL);
+            System.out.println("Table \"address\" is created!");
         } catch (SQLException e) {
-            System.out.println (e.getMessage ());
+            System.out.println(e.getMessage());
         } finally {
             if (statement != null) {
                 try {
-                    statement.close ();
+                    statement.close();
                 } catch (SQLException e) {
-                    e.printStackTrace ();
+                    e.printStackTrace();
                 }
             }
         }
@@ -192,25 +179,28 @@ public class DBUtils {
         Statement statement = null;
 
         String createTableSQL = "CREATE TABLE telephone_numbers("
-                + "telephone_numbers_id INTEGER NOT NULL AUTO_INCREMENT, "
-                + "tel_number VARCHAR(13) NOT NULL, "
-                + "user_id INTEGER NOT NULL, "
-                + "PRIMARY KEY (telephone_numbers_id) "
+                + "telephone_number_id INTEGER NOT NULL, "
+                + "tel_number VARCHAR(20) NOT NULL, "
+                + "PRIMARY KEY (telephone_number_id) "
+                + "FOREIGN KEY (user_id) REFERENCES contact(user_id)  " +
+                "ON DELETE CASCADE " +
+                "ON UPDATE CASCADE "
                 + ")";
+
         try {
-            statement = dbConnection.createStatement ();
+            statement = dbConnection.createStatement();
 
             // выполнить SQL запрос
-            statement.execute (createTableSQL);
-            System.out.println ("Table \"dbuser\" is created!");
+            statement.execute(createTableSQL);
+            System.out.println("Table \"telephone_numbers\" is created!");
         } catch (SQLException e) {
-            System.out.println (e.getMessage ());
+            System.out.println(e.getMessage());
         } finally {
             if (statement != null) {
                 try {
-                    statement.close ();
+                    statement.close();
                 } catch (SQLException e) {
-                    e.printStackTrace ();
+                    e.printStackTrace();
                 }
             }
         }
@@ -220,42 +210,30 @@ public class DBUtils {
         Statement statement = null;
 
         String createTableSQL = "CREATE TABLE emails("
-                + "email_id INTEGER NOT NULL AUTO_INCREMENT, "
-                + "email VARCHAR(13) NOT NULL, "
-                + "user_id INTEGER NOT NULL, "
+                + "email_id INTEGER NOT NULL, "
+                + "email VARCHAR(100) NOT NULL, "
                 + "PRIMARY KEY (email_id) "
+                + "FOREIGN KEY (user_id) REFERENCES contact(user_id) " +
+                "ON DELETE CASCADE" +
+                "ON UPDATE CASCADE"
                 + ")";
+
         try {
-            statement = dbConnection.createStatement ();
+            statement = dbConnection.createStatement();
 
             // выполнить SQL запрос
-            statement.execute (createTableSQL);
-            System.out.println ("Table \"dbuser\" is created!");
+            statement.execute(createTableSQL);
+            System.out.println("Table \"emails\" is created!");
         } catch (SQLException e) {
-            System.out.println (e.getMessage ());
+            System.out.println(e.getMessage());
         } finally {
             if (statement != null) {
                 try {
-                    statement.close ();
+                    statement.close();
                 } catch (SQLException e) {
-                    e.printStackTrace ();
+                    e.printStackTrace();
                 }
             }
-        }
-    }
-
-    static void delete() {
-        String deleteTableSQL = "DELETE DBUSER WHERE USER_ID = 1";
-
-        try {
-            Connection dbConnection = getConnection ();
-            Statement statement = dbConnection.createStatement ();
-
-            // выполняем запрос delete SQL
-            statement.execute (deleteTableSQL);
-            System.out.println ("Record is deleted from DBUSER table!");
-        } catch (SQLException e) {
-            System.out.println (e.getMessage ());
         }
     }
 }
--- src/thread/HelloRunnable.java
+++ src/thread/HelloRunnable.java
@@ -2,12 +2,23 @@ package thread;
 
 public class HelloRunnable implements Runnable {
 
+
     public void run() {
         System.out.println("Hello from a thread!");
     }
 
     public static void main(String args[]) {
-        (new Thread(new HelloRunnable())).start();
+        /*Thread thread =  new Thread(new HelloRunnable());
+        thread.start();
+
+        thread.start();*/
+        HelloRunnable target = new HelloRunnable();
+
+        for (int i = 0; i < 10; i++) {
+
+            Thread thread = new Thread(target);
+            thread.start();
+        }
     }
 
 }
